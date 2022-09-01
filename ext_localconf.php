8f0e37cb76a6401eae9d2c3b84ca55a48342113a
ffassing <ffassing@mediengarage.de>
2022-09-01 14:07:40 +0200

    changed label of timeline content element

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,5 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fk_sitepackage'] = 'EXT:fk_sitepa
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fk_sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
+// Add Content Elements
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fk_sitepackage/Configuration/TsConfig/Page/ContentElement/All.tsconfig">');
